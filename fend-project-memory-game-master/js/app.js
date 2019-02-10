b42f65b6ae2b54bd03cfbdf7ecc7ebf785eafd39
Luis <luisbull@hotmail.com>
2019-02-10 21:37:17 +0000

    Created a list that holds all of your cards
    Brought the card names from HTML file into an array

--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -1,7 +1,16 @@
 /*
  * Create a list that holds all of your cards
+ * Brought the card names from HTML file into an array
  */
 
+var icons = ["fa fa-diamond", "fa fa-diamond",
+              "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+              "fa fa-anchor", "fa fa-anchor",
+              "fa fa-bolt", "fa fa-bolt",
+              "fa fa-cube", "fa fa-cube",
+              "fa fa-leaf", "fa fa-leaf",
+              "fa fa-bicycle", "fa fa-bicycle",
+              "fa fa-bomb", "fa fa-bomb"];
 
 /*
  * Display the cards on the page
